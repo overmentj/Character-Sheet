b0afe38a0cdda09108f314ae2fca2647f6d0062f
Robin Moss <robin_moss@btinternet.com>
2012-02-01 23:13:41 +0000

    Sleepy time for robin so committing my stuff

--- Character Sheet/src/com/teabreak/gui/charactersheet/CharacterSheet.java
+++ Character Sheet/src/com/teabreak/gui/charactersheet/CharacterSheet.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JLayeredPane;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JLabel;
 
 public class CharacterSheet extends JFrame {
 
@@ -32,11 +37,38 @@ public class CharacterSheet extends JFrame {
 	 */
 	public CharacterSheet() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 873, 777);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{847, 0};
+		gbl_contentPane.rowHeights = new int[]{729, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		GridBagConstraints gbc_tabbedPane = new GridBagConstraints();
+		gbc_tabbedPane.fill = GridBagConstraints.BOTH;
+		gbc_tabbedPane.gridx = 0;
+		gbc_tabbedPane.gridy = 0;
+		contentPane.add(tabbedPane, gbc_tabbedPane);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Front Page", null, tabbedPane_1, null);
+		
+		JTabbedPane tabbedPane_2 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("Inventory", null, tabbedPane_2, null);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		tabbedPane.addTab("Page 3", null, layeredPane, null);
+		
+		JLayeredPane layeredPane_1 = new JLayeredPane();
+		tabbedPane.addTab("Skills & Feats", null, layeredPane_1, null);
+		
+		JLabel lblNewLabel = new JLabel("Ability Scores");
+		lblNewLabel.setBounds(10, 86, 99, 14);
+		layeredPane_1.add(lblNewLabel);
 	}
-
 }
