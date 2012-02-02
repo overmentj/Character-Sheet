238f9967a61766fd6d009f48d348cd0580947461
Veritas546 <phoenix546@gmail.com>
2012-02-02 21:18:04 +0000

    Added the variables for Weapons and Armour

--- Character Sheet/src/com/teabreak/core/aspects/equipment/Armour.java
+++ Character Sheet/src/com/teabreak/core/aspects/equipment/Armour.java
@@ -12,10 +12,53 @@ public class Armour extends EquipmentAbstract {
 
 	private final EquipmentEnum EQUIPMENT_TYPE = EquipmentEnum.Armour;
 	
+	private int armorBonus = 0;
+	private int maxDexBonus = 0;
+	private int checkPenalty = 0;
+	private int spellFailure = 0;
+	private int weight = 0;
+	private int maxSpeed = 0;
+	private int minSpeed = 0;
+	private int goldValue = 0;
+	
+	private String name;
+	
+	public int getArmorBonus() {
+		return armorBonus;
+	}
+	
+	public int getMaxDexBonus() {
+		return maxDexBonus;
+	}
+	
+	public int getCheckPenalty() {
+		return checkPenalty;
+	}
+	
+	public int getSpellFailure() {
+		return spellFailure;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public int getMinSpeed() {
+		return minSpeed;
+	}
+	
+	public int getGoldValue() {
+		return goldValue;
+	}
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
--- Character Sheet/src/com/teabreak/core/aspects/equipment/Weapon.java
+++ Character Sheet/src/com/teabreak/core/aspects/equipment/Weapon.java
@@ -6,11 +6,58 @@ public class Weapon extends EquipmentAbstract {
 
 	private final EquipmentEnum EQUIPMENT_TYPE = EquipmentEnum.Weapon;
 	
+	private int damageSmall = 0;
+	private int damageMed = 0;
+	private int critical = 0;
+	private boolean critMultiple = false;
+	private int rangeInc = 0;
+	private int weight = 0;
+	private boolean bludgeoning = false;
+	private boolean slashing = false;
+	private boolean piercing = false;
+	
+	private String name;
+	
+	public int getDamageSmall() {
+		return damageSmall;
+	}
+	
+	public int getDamageMed() {
+		return damageMed;
+	}
+	
+	public int getCritical() {
+		return critical;
+	}
+	
+	public boolean getCritMultiple() {
+		return critMultiple;
+	}
+	
+	public int getRangeInc() {
+		return rangeInc;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public boolean getBludgeoning() {
+		return bludgeoning;
+	}
+	
+	public boolean getSlashing() {
+		return slashing;
+	}
+	
+	public boolean getPiercing() {
+		return piercing;
+	}
 	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 
