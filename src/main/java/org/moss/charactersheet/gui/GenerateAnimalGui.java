1faaf61d142aed9a3b7e25fb4c6dab2955fa8466
JacqMT <jacqtrain@gmail.com>
2014-12-10 17:09:39 +0000

    Issue #7 Further work on Animal Companion screen

--- src/main/java/org/moss/charactersheet/gui/GenerateAnimalGui.java
+++ src/main/java/org/moss/charactersheet/gui/GenerateAnimalGui.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.BorderFactory;
@@ -14,6 +15,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.moss.charactersheet.aspects.enums.AbilityScore;
+import org.moss.charactersheet.aspects.enums.Save;
 
 /**
  * Generate GUI for Animal Companion
@@ -91,6 +93,7 @@ public class GenerateAnimalGui implements GenerateGui {
 		
 		JLabel lblInit = new JLabel("Initiative");
 		JTextField textInit = new JTextField(30);
+		textInit.setAlignmentX(Component.LEFT_ALIGNMENT);
 		JLabel lblSpeed = new JLabel("Speed");
 		JTextField textSpeed = new JTextField(30);
 		
@@ -122,23 +125,173 @@ public class GenerateAnimalGui implements GenerateGui {
 
 	private void addAC() {
 		GenerateAcGui acGUI = new GenerateAcGui();
-		animCons.gridy = 2;
+		animCons.gridy++;
 		animalInfo.add(acGUI.generate(), animCons);		
 	}
 
 	private void addAttackStats() {
-		// TODO Auto-generated method stub
+		JPanel stats = new JPanel(new GridBagLayout());
+		GridBagConstraints statCons = new GridBagConstraints();
 		
+		JPanel saves = new JPanel(new GridBagLayout());
+		saves.setBorder(BorderFactory.createTitledBorder("Saving Throws"));
+		GridBagConstraints savCons = new GridBagConstraints();
+		
+		savCons.gridy = 0;
+		for (Save save : Save.values()) {
+			savCons.gridx = 0;
+			JLabel lblSave = new JLabel(save.getSaveName());
+			JTextField textSave = new JTextField(3);
+			textSave.setEditable(false);
+			saves.add(lblSave, savCons);
+			savCons.gridx++;
+			saves.add(textSave, savCons);
+			savCons.gridy++;
+		}
+		stats.add(saves);
+		
+		JPanel attacks = new JPanel(new GridBagLayout());
+		attacks.setBorder(BorderFactory.createTitledBorder("Attacks"));
+		GridBagConstraints attCons = new GridBagConstraints();
+		
+		List<String> labels = Arrays.asList("Weapon", "Bonus", "Damage", "Critical");
+		
+		attCons.gridy = 0;
+		for (int i = 0; i < 3; i++) {
+			attCons.gridx = 0;
+			for (String label : labels) {
+				JLabel lblAttStat = new JLabel(label);
+				attacks.add(lblAttStat, attCons);
+				attCons.gridx++;
+			}
+			attCons.gridy++;
+			attCons.gridx = 0;
+			for (int j = 0; j < 4; j++) {
+				JTextField textNum = new JTextField(10);
+				attacks.add(textNum, attCons);
+				attCons.gridx++;
+			}
+			
+			attCons.gridy++;
+		}
+		statCons.gridx = 1;
+		stats.add(attacks, statCons);
+		
+		JPanel grapp = new JPanel(new GridBagLayout());
+		grapp.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints grapCons = new GridBagConstraints();
+		
+		JLabel lblGrap = new JLabel("Grapple");
+		JLabel lblPers = new JLabel("Personality");
+		JTextField textGrap = new JTextField(15);
+		JTextField textPers = new JTextField(15);
+		
+		grapp.add(lblGrap);
+		grapCons.gridx = 1;
+		grapp.add(textGrap, grapCons);
+		grapCons.gridx--;
+		grapCons.gridy = 1;
+		grapp.add(lblPers, grapCons);
+		grapCons.gridx++;
+		grapp.add(textPers, grapCons);
+		
+		statCons.gridx++;
+		stats.add(grapp, statCons);
+		
+		animCons.gridy++;
+		animalInfo.add(stats, animCons);
 	}
 
 	private void addSkills() {
 		// TODO Auto-generated method stub
-		addAbilities();
+		JPanel bigPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints bpCons = new GridBagConstraints();
+		
+		JPanel skills = new JPanel(new GridBagLayout());
+		skills.setBorder(BorderFactory.createTitledBorder("Skills"));
+		GridBagConstraints skCons = new GridBagConstraints();
+		
+		skCons.gridy = 0;
+		for (int i = 0; i < 10; i++) {
+			skCons.gridx = 0;
+			JTextField textSkill = new JTextField(15);
+			JLabel lblPlus = new JLabel(" + ");
+			JTextField textMod = new JTextField(3);
+			
+			skills.add(textSkill, skCons);
+			skCons.gridx++;
+			skills.add(lblPlus, skCons);
+			skCons.gridx++;
+			skills.add(textMod, skCons);
+			
+			skCons.gridy++;
+		}
+		bigPanel.add(skills);
+		addAbilities(bigPanel, bpCons);
+		
+		animCons.gridy++;
+		animalInfo.add(bigPanel, animCons);
 	}
 
-	private void addAbilities() {
-		// TODO Auto-generated method stub
-		// Feats, Abilities, Tricks
-	}
+	private void addAbilities(JPanel parentPanel, GridBagConstraints parentCons) {
+		// | Feats            |
+		// |------------------|
+		// |Abilities, Tricks |
+		JPanel bigPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints bpCons = new GridBagConstraints();
+		
+		// Feats
+		JPanel feats = new JPanel(new GridBagLayout());
+		feats.setBorder(BorderFactory.createTitledBorder("Feats"));
+		GridBagConstraints fCons = new GridBagConstraints();
+		
+		fCons.gridy = 0;
+		fCons.gridx = 0;
+		for (int i = 0; i < 6; i++) {
+			JTextField textFeat = new JTextField(20);
+			if (i != 0 && i % 2 == 0) {
+				fCons.gridx = 0;
+				fCons.gridy++;
+			}
+			feats.add(textFeat, fCons);
+			fCons.gridx++;
+		}
+		bigPanel.add(feats);
+		bpCons.gridy = 1;
+		
+		// Special Abilities
+		JPanel miniPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints miniCons = new GridBagConstraints();
+		
+		JPanel specAbs = new JPanel(new GridBagLayout());
+		specAbs.setBorder(BorderFactory.createTitledBorder("Special Abilities"));
+		GridBagConstraints saCons = new GridBagConstraints();
+		
+		saCons.gridx = 0;
+		saCons.gridy = 0;
+		for (int i = 0; i < 6; i++) {
+			JTextField abil = new JTextField(20);
+			specAbs.add(abil, saCons);
+			saCons.gridy++;
+		}
+		miniPanel.add(specAbs);
 
+		// Tricks
+		JPanel tricks = new JPanel(new GridBagLayout());
+		tricks.setBorder(BorderFactory.createTitledBorder("Tricks"));
+		GridBagConstraints trCons = new GridBagConstraints();
+		trCons.gridx = 0;
+		trCons.gridy = 0;
+		for (int i = 0; i < 6; i++) {
+			JTextField trick = new JTextField(20);
+			tricks.add(trick, trCons);
+			trCons.gridy++;
+		}
+		miniCons.gridx = 1;
+		miniPanel.add(tricks, miniCons);
+		bigPanel.add(miniPanel, bpCons);
+		
+		parentCons.gridx = 1;
+		parentPanel.add(bigPanel, parentCons);
+	}
 }
